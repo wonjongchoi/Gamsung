bcbe622039ca6030c56f7539878c35d00d0f19f5
jyoung <violentjy@gmail.com>
2016-12-13 00:41:56 +0900

    감정 추가 화면에서 텍스트 입력 창 누르면 입력 창이 키보드에 밀려올라가 보이도록 수정

--- EmoDiary/EmoDiary/CreateViewController.swift
+++ EmoDiary/EmoDiary/CreateViewController.swift
@@ -11,22 +11,15 @@ import UIKit
 class CreateViewController: UIViewController {
     var selectEmo:EmotionIndex = .love
 
+    @IBOutlet weak var scroll: UIScrollView!
+    
+    @IBOutlet var colorBtns: [UIButton]!
+    
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var inputMemo: UITextField!
     
-    @IBOutlet weak var love: UIButton!
-    @IBOutlet weak var lonely: UIButton!
-    @IBOutlet weak var calm: UIButton!
-    @IBOutlet weak var relieved: UIButton!
-    @IBOutlet weak var sad: UIButton!
-    @IBOutlet weak var shame: UIButton!
-    @IBOutlet weak var fun: UIButton!
-    @IBOutlet weak var feelingless: UIButton!
-    @IBOutlet weak var anger: UIButton!
-    @IBOutlet weak var happy: UIButton!
-    
     @IBAction func pickEmotion(_ sender: UIButton) {
-        nogadaBtnSet()
+        btnSet()
         sender.setTitleColor(UIColor.black, for: .normal)
         
         selectEmo = EmotionIndex(rawValue: sender.tag)!
@@ -50,15 +43,18 @@ class CreateViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.tabBarController?.tabBar.isHidden = true
         
-        nogadaBtnSet()
-        
-        self.love.setTitleColor(UIColor.black, for: .normal)
-        
         datePicker.maximumDate = Date.init()
-        
 
         NotificationCenter.default.addObserver(self, selector: #selector(CreateViewController.keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(CreateViewController.keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+        
+        btnSet()
+        
+        for colorBtn in colorBtns {
+            if colorBtn.tag == EmotionIndex.love.rawValue {
+                colorBtn.setTitleColor(UIColor.black, for: .normal)
+            }
+        }
         
     }
 
@@ -83,60 +79,41 @@ class CreateViewController: UIViewController {
         let keyboardEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         
         // 스크롤 업 (텍스트 필드를 키보드 제외한 화면의 y 중간까지 스크롤
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardEndFrame.height, 0.0)
+        scroll.contentInset = contentInsets
+        scroll.scrollIndicatorInsets = contentInsets
         
+        let textFieldOrigin:CGPoint = self.inputMemo.frame.origin;
+        let textFieldHeight:CGFloat = self.inputMemo.frame.size.height;
+        
+        var aRect:CGRect = self.view.frame
+        aRect.size.height -= keyboardEndFrame.height
+        
+        if (!aRect.contains(textFieldOrigin))
+        {
+            //you can add yor desired height how much you want move keypad up, by replacing "textFieldHeight" below
+            
+            let scrollPoint = CGPoint.init(x: 0.0, y: textFieldOrigin.y - aRect.size.height  + textFieldHeight) //replace textFieldHeight to currentKeyboardSize.height, if you want to move up with more height
+            scroll.setContentOffset(scrollPoint, animated: true)
+        }
     }
     
     func keyboardWillHide(_ notification: Notification) {
         let userInfo = notification.userInfo!
         
         //스크롤 다운
-        
+        scroll.setContentOffset(CGPoint.zero, animated: true)
     }
     
-    func nogadaBtnSet() {
-        self.happy.setTitle(emoArray[.happy]?.name, for: .normal)
-        self.happy.setTitleColor(UIColor.white, for: .normal)
-        self.happy.layer.cornerRadius = self.happy.fs_width / 2
-        self.happy.backgroundColor = hexStringToUIColor(hex: (emoArray[.happy]?.resource)!)
-        self.love.setTitle(emoArray[.love]?.name, for: .normal)
-        self.love.setTitleColor(UIColor.white, for: .normal)
-        self.love.layer.cornerRadius = self.love.fs_width / 2
-        self.love.backgroundColor = hexStringToUIColor(hex: (emoArray[.love]?.resource)!)
-        self.relieved.setTitle(emoArray[.relieved]?.name, for: .normal)
-        self.relieved.setTitleColor(UIColor.white, for: .normal)
-        self.relieved.layer.cornerRadius = self.relieved.fs_width / 2
-        self.relieved.backgroundColor = hexStringToUIColor(hex: (emoArray[.relieved]?.resource)!)
-        self.fun.setTitle(emoArray[.fun]?.name, for: .normal)
-        self.fun.setTitleColor(UIColor.white, for: .normal)
-        self.fun.layer.cornerRadius = self.fun.fs_width / 2
-        self.fun.backgroundColor = hexStringToUIColor(hex: (emoArray[.fun]?.resource)!)
-        self.anger.setTitle(emoArray[.anger]?.name, for: .normal)
-        self.anger.setTitleColor(UIColor.white, for: .normal)
-        self.anger.layer.cornerRadius = self.anger.fs_width / 2
-        self.anger.backgroundColor = hexStringToUIColor(hex: (emoArray[.anger]?.resource)!)
-        self.sad.setTitle(emoArray[.sad]?.name, for: .normal)
-        self.sad.setTitleColor(UIColor.white, for: .normal)
-        self.sad.layer.cornerRadius = self.sad.fs_width / 2
-        self.sad.backgroundColor = hexStringToUIColor(hex: (emoArray[.sad]?.resource)!)
-        self.lonely.setTitle(emoArray[.lonely]?.name, for: .normal)
-        self.lonely.setTitleColor(UIColor.white, for: .normal)
-        self.lonely.layer.cornerRadius = self.lonely.fs_width / 2
-        self.lonely.backgroundColor = hexStringToUIColor(hex: (emoArray[.lonely]?.resource)!)
-        self.shame.setTitle(emoArray[.shame]?.name, for: .normal)
-        self.shame.setTitleColor(UIColor.white, for: .normal)
-        self.shame.layer.cornerRadius = self.shame.fs_width / 2
-        self.shame.backgroundColor = hexStringToUIColor(hex: (emoArray[.shame]?.resource)!)
-        self.calm.setTitle(emoArray[.calm]?.name, for: .normal)
-        self.calm.setTitleColor(UIColor.white, for: .normal)
-        self.calm.layer.cornerRadius = self.calm.fs_width / 2
-        self.calm.backgroundColor = hexStringToUIColor(hex: (emoArray[.calm]?.resource)!)
-        self.feelingless.setTitle(emoArray[.feelingless]?.name, for: .normal)
-        self.feelingless.setTitleColor(UIColor.white, for: .normal)
-        self.feelingless.layer.cornerRadius = self.feelingless.fs_width / 2
-        self.feelingless.backgroundColor = hexStringToUIColor(hex: (emoArray[.feelingless]?.resource)!)
+    func btnSet() {
+        for colorBtn in colorBtns {
+            colorBtn.setTitle(emoArray[EmotionIndex(rawValue:colorBtn.tag)!]?.name, for: .normal)
+            colorBtn.setTitleColor(UIColor.white, for: .normal)
+            colorBtn.layer.cornerRadius = colorBtn.fs_width / 2
+            colorBtn.backgroundColor = hexStringToUIColor(hex: (emoArray[EmotionIndex(rawValue:colorBtn.tag)!]?.resource)!)
+        }
     }
     
-
     /*
     // MARK: - Navigation
 
